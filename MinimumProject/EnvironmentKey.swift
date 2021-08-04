f22672345b3f920595dacaee5e444e058c536de1
Yuta Saito <kateinoigakukun@gmail.com>
2021-08-04 06:17:29 -0700

    WIP

--- MinimumProject/EnvironmentKey.swift
+++ MinimumProject/EnvironmentKey.swift
@@ -1,17 +1,3 @@
-// Copyright 2020 Tokamak contributors
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 public protocol EnvironmentKey {
   associatedtype Value
   static var defaultValue: Value { get }
@@ -20,11 +6,6 @@ public protocol EnvironmentKey {
 public struct _EnvironmentKeyWritingModifier<Value> {
   public let keyPath: WritableKeyPath<EnvironmentValues, Value>
   public let value: Value
-
-  public init(keyPath: WritableKeyPath<EnvironmentValues, Value>, value: Value) {
-    self.keyPath = keyPath
-    self.value = value
-  }
 }
 
 public extension View {
