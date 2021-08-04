47eba4b2cd397e86f2d092a40dccde1ebb135692
Yuta Saito <kateinoigakukun@gmail.com>
2021-08-04 06:24:19 -0700

    WIP

--- MinimumProject/Color.swift
+++ MinimumProject/Color.swift
@@ -5,16 +5,32 @@ struct AccentColorKey: EnvironmentKey {
   static let defaultValue: Color? = nil
 }
 
-public extension EnvironmentValues {
+extension EnvironmentValues {
   var accentColor: Color? {
-    get {
-      self[AccentColorKey.self]
-    }
-    set {
-      self[AccentColorKey.self] = newValue
-    }
+    get { return nil }
+    set {}
   }
 }
+
+public protocol EnvironmentKey {
+    associatedtype Value
+    static var defaultValue: Value { get }
+}
+
+public struct _EnvironmentKeyWritingModifier<Value> {
+    public let keyPath: WritableKeyPath<EnvironmentValues, Value>
+    public let value: Value
+}
+
+public extension View {
+  func environment<V>(
+    _ keyPath: WritableKeyPath<EnvironmentValues, V>,
+    _ value: V
+  ) {
+    modifier(_EnvironmentKeyWritingModifier(keyPath: keyPath, value: value))
+  }
+}
+
 
 public extension View {
   func accentColor(_ accentColor: Color?) {
