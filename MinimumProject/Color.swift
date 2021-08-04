c9c7291ea557a4995cceeb6a376c3917528e9dc6
Yuta Saito <kateinoigakukun@gmail.com>
2021-08-04 06:29:45 -0700

    WIP

--- MinimumProject/Color.swift
+++ MinimumProject/Color.swift
@@ -1,15 +1,6 @@
 public protocol View {
 }
 
-public extension View {
-  func modifier<Modifier>(_ modifier: Modifier) {
-  }
-}
-
-
-
-
-
 public struct Color {
 }
 
@@ -29,17 +20,16 @@ public protocol EnvironmentKey {
     static var defaultValue: Value { get }
 }
 
-public struct _EnvironmentKeyWritingModifier<Value> {
-    public let keyPath: WritableKeyPath<EnvironmentValues, Value>
-    public let value: Value
+struct _EnvironmentKeyWritingModifier<Value> {
+    let keyPath: WritableKeyPath<EnvironmentValues, Value>
 }
 
-public extension View {
+extension View {
   func environment<V>(
     _ keyPath: WritableKeyPath<EnvironmentValues, V>,
     _ value: V
   ) {
-    modifier(_EnvironmentKeyWritingModifier(keyPath: keyPath, value: value))
+    _EnvironmentKeyWritingModifier(keyPath: keyPath)
   }
 }
 
